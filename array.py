3f4c20f0e27f2a1b3084675bc3e2857454cfbd0d
caroix <caroline.fdf1@icloud.com>
2025-09-01 20:14:15 -0300

    Update array.py

--- array.py
+++ array.py
@@ -3,7 +3,7 @@ colegas = ["Suzane", "Mirelly", "Leonardo", "Vinicius", "Marcos Vinicius"]
 
 print("Primeiro elemento:", colegas[0])
 print("Terceiro elemento:", colegas[2])
-print("Ultimo elemento:", colegas[-1])
+print("Ultimo elemento:", colegas[4])
 
 if "Marcos Vinicius" in colegas:
    print("Marcos Vinicius está na lista!")
