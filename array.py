05865b5d81bb201f4ebe3dce65b21a71fb205d88
caroix <caroline.fdf1@icloud.com>
2025-09-01 19:44:40 -0300

    Update array.py

--- array.py
+++ array.py
@@ -1,1 +1,11 @@
+#Lista com 5 nomes de colegas
+colegas = ["Suzane", "Mirelly", "Leonardo", "Vinicius", "Marcos Vinicius"]
 
+print("Primeiro elemento:", colegas[0])
+print("Terceiro elemento:", colegas[2])
+print("Ultimo elemento:", colegas[-1])
+
+if "Marcos Vinicius" in colegas:
+   print("Marcos Vinicius está na lista!")
+else:
+    print("Marcos Vinicius não foi encontrado na lista.")
